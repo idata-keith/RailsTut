31ef9bc89f0fddd2e85b93a5f2b8abfa6b535094
Keith  Wright <Keith.wright13@gmail.com>
2015-07-20 12:02:49 -0400

    5.2 Creating a form

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+get 'welcome/index'
 
-  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
+  resources :articles
+
    root 'welcome#index'
 
   # Example of regular route:
